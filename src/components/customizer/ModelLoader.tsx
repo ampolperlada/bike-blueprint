a475870773b165d0ec2819b41a855bbbaa46a2ec
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2026-03-14 13:13:55 +0800

    fix(build): add useRef initial value and update Three.js imports

--- src/components/customizer/ModelLoader.tsx
+++ src/components/customizer/ModelLoader.tsx
@@ -5,7 +5,7 @@
 
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 interface ModelLoaderProps {
   colors: {
